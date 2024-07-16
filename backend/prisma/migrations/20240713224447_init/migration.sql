ee57090207ce7dfe9418acae19f519bde0679bc6
Betty Nguyen <bnguyen47@my.bcit.ca>
2024-07-16 01:39:21 -0700

    Refactor and combine sql migrations

--- backend/prisma/migrations/20240713224447_init/migration.sql
+++ backend/prisma/migrations/20240713224447_init/migration.sql
@@ -1,4 +1,6 @@
--- CreateTable
+PRAGMA foreign_keys=off;
+
+-- Create Tables
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "userName" TEXT NOT NULL,
@@ -10,7 +12,6 @@ CREATE TABLE "User" (
     "photoURL" TEXT
 );
 
--- CreateTable
 CREATE TABLE "Cafe" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "stringId" TEXT NOT NULL,
@@ -24,12 +25,11 @@ CREATE TABLE "Cafe" (
     "profilePhotoURL" TEXT,
     "busyness" TEXT,
     "noisiness" TEXT,
-    FOREIGN KEY ("province") REFERENCES "ProvinceCode" ("code"),
-    FOREIGN KEY ("busyness") REFERENCES "BusynessOption" ("level"),
-    FOREIGN KEY ("noisiness") REFERENCES "NoisinessOption" ("level")
+    CONSTRAINT "Cafe_province_fkey" FOREIGN KEY ("province") REFERENCES "ProvinceCode" ("code") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Cafe_busyness_fkey" FOREIGN KEY ("busyness") REFERENCES "BusynessOption" ("level") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Cafe_noisiness_fkey" FOREIGN KEY ("noisiness") REFERENCES "NoisinessOption" ("level") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
--- CreateTable
 CREATE TABLE "Location" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "latitude" REAL NOT NULL,
@@ -38,30 +38,30 @@ CREATE TABLE "Location" (
     CONSTRAINT "Location_cafeId_fkey" FOREIGN KEY ("cafeId") REFERENCES "Cafe" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
--- CreateTable
-CREATE TABLE "FilterOption" (
+CREATE TABLE "ProvinceCode" (
+    "code" TEXT NOT NULL PRIMARY KEY,
+    "country" TEXT NOT NULL
+);
+
+CREATE TABLE "BusynessOption" (
     "level" TEXT NOT NULL PRIMARY KEY
 );
 
--- CreateTable
-CREATE TABLE "ProvinceCode" (
-    "code" TEXT NOT NULL PRIMARY KEY
+CREATE TABLE "NoisinessOption" (
+    "level" TEXT NOT NULL PRIMARY KEY
 );
 
--- CreateIndex
+-- Create indexes
 CREATE UNIQUE INDEX "User_userName_key" ON "User"("userName");
 
--- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
--- CreateIndex
 CREATE UNIQUE INDEX "Cafe_stringId_key" ON "Cafe"("stringId");
 
--- CreateIndex
 CREATE UNIQUE INDEX "Location_latitude_key" ON "Location"("latitude");
 
--- CreateIndex
 CREATE UNIQUE INDEX "Location_longitude_key" ON "Location"("longitude");
 
--- CreateIndex
 CREATE UNIQUE INDEX "Location_cafeId_key" ON "Location"("cafeId");
+
+PRAGMA foreign_keys=on;
