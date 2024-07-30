52117794291e34f6739e6dd1b39cf97315ad0cfe
Betty Nguyen <bnguyen47@my.bcit.ca>
2024-07-30 01:15:00 -0700

    render menu icon on mobile only

--- spire-coffee/src/component/NavBar.tsx
+++ spire-coffee/src/component/NavBar.tsx
@@ -20,6 +20,13 @@ import strings from "@/config/strings";
 import "../index.css";
 
 const NavBar = () => {
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen: boolean) => () => {
@@ -32,8 +39,8 @@ const NavBar = () => {
     navigate(path);
   };
 
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+  const drawer = (
+    <Box sx={{ width: 250, textAlign: 'center' }} role="presentation" onClick={handleDrawerToggle}>
       <List>
         {NavBarItems.map(({ label, path }) => (
           <ListItem key={label} disablePadding>
@@ -49,21 +56,21 @@ const NavBar = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" component="nav">
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={toggleDrawer(true)}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+            onClick={handleDrawerToggle}
           >
             <MenuIcon />
           </IconButton>
-          <Drawer open={open} onClose={toggleDrawer(false)}>
+          {/* <Drawer open={open} onClose={toggleDrawer(false)}>
             {DrawerList}
-          </Drawer>
+          </Drawer> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {strings.general.title}
           </Typography>
@@ -83,6 +90,22 @@ const NavBar = () => {
           </Box>
         </Toolbar>
       </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box'},
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
     </Box>
   );
 };
