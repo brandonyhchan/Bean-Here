15832d8a4b19201541bdaafc02696250f065a142
Izabelle Datayan <i_datayan@yahoo.com>
2024-08-01 13:23:48 -0700

    SC-25 exported theme to be under src/styles

--- spire-coffee/src/App.tsx
+++ spire-coffee/src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+import mainTheme from './styles/mainTheme';
 import Body from "./component/Body";
 import { AuthProvider } from "./context/AuthContext";
 import SignUp from "./pages/Signup";
@@ -12,58 +13,11 @@ import Favourites from "./pages/Favourites";
 import AddCafe from "./pages/AddCafe";
 import Account from "./pages/Account";
 
-// as we decide on colors, font sizes and other design choices they can go here
-const theme = createTheme({
-  palette: {
-    primary: { // tan colours
-      main: "#a1887f",
-      light: "#a1887fa4",
-      dark: "#8b7972"
-    },
-    secondary: { // blue colours
-      main: "#3f51b5",
-      light: "#bec8ff",
-      dark: "#344397"
-    },
-    warning: {
-      main: "rgb(255, 32, 32)",
-    },
-  },
-  typography: {
-    fontFamily: "Figtree-Regular",
-    fontSize: 14,
-    h1: {
-      fontSize: "3.2rem",
-      lineHeight: "1.1",
-    },
-    h2: {
-      fontSize: "3.75rem",
-    },
-    h3: {
-      fontSize: "2.3rem",
-    },
-    h4: {
-      fontSize: "2.125rem",
-    },
-    h5: {
-      fontSize: "20px", // This is the "large font" from old spire coffee
-    },
-    h6: {
-      fontSize: "18px", // This is the "medium font" from old spire coffee
-    },
-    button: {
-      fontSize: "1em",
-      fontWeight: 500,
-      fontFamily: "Figtree-Regular",
-    },
-  },
-});
-
 function App() {
   return (
     <div className="App">
       <Body>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={mainTheme}>
           <AuthProvider>
             <Routes>
               <Route path="/signup" element={<SignUp />} />
