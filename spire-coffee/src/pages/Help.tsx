934a82ef019ce7aaba88b09e69d6270065ddd50f
Betty Nguyen <97364242+bettyanhn@users.noreply.github.com>
2024-08-03 17:12:47 -0700

    Sc 31 fix footer and route (#17)
    * fix bug with refresh for routing
    
    * add routing for extra pages

--- spire-coffee/src/pages/Help.tsx
+++ spire-coffee/src/pages/Help.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 const Help = () => {
   return (
     <React.Fragment>
-      <h1>Help us $$$$$$$</h1>
+      <h1>Help us pwease</h1>
     </React.Fragment>
   );
 };
