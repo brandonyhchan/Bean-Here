4f7c76a1331451475a26da5739acb8064363e407
Betty Nguyen <bnguyen47@my.bcit.ca>
2024-08-03 17:40:23 -0700

    add cafe card from old repo

---
+++ spire-coffee/src/component/card/CafeCard.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import {
+  renderBusyIcon,
+  renderNoiseIcon,
+  renderPrice,
+} from "../icon/Icons";
+import classNames from "classnames";
+import strings from "../../config/strings";
+import styles from "./cafeCard.module.scss";
+
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+type CafeCardPropsType = {
+  id: number;
+  name: string;
+  street: string;
+  city: string;
+  province: string;
+  profilePhotoURL: string;
+  busyness: string;
+  noisiness: string;
+  price: string;
+};
+
+const CafeCard = ({
+  name,
+  street,
+  city,
+  province,
+  profilePhotoURL,
+  busyness,
+  noisiness,
+  price,
+}: CafeCardPropsType) => {
+  const mobileSize = useMediaQuery("(min-width) and (max-width:600px)");
+  const xsMobileSize = useMediaQuery("(max-width:299px)");
+
+  function checkSize() {
+    if (xsMobileSize) {
+      return 14;
+    } else {
+      return 20;
+    }
+  }
+
+  return (
+    <div className={classNames(styles.cafeCard)}>
+      <div className={classNames(styles.logo)}>
+        <img src={profilePhotoURL} />
+      </div>
+      <div className={classNames(styles.cafeCardInfo)}>
+        <Grid item zeroMinWidth>
+          <Typography
+            className={classNames(styles.mobilefont)}
+            noWrap
+            fontFamily={"Figtree-Regular"}
+            fontSize={mobileSize ? 15 : checkSize}
+            fontWeight={"600"}
+          >
+            {name}
+          </Typography>
+          <Typography
+            fontSize={mobileSize ? 15 : checkSize}
+            fontFamily={"Figtree-Regular"}
+          >
+            {street}
+          </Typography>
+          <Typography
+            fontSize={mobileSize ? 15 : checkSize}
+            fontFamily={"Figtree-Regular"}
+          >
+            {city}, {province}
+          </Typography>
+        </Grid>
+        <div className={classNames(styles.cafeCardIcons)}>
+          <div className={classNames(styles.iconLabel)}>
+            {renderBusyIcon(busyness)}
+            <div>
+              <label>Busy</label>
+            </div>
+          </div>
+          <div className={classNames(styles.iconLabel)}>
+            {renderNoiseIcon(noisiness)}
+            <label className={classNames(styles.noisinessLabel)}>
+              Noise
+            </label>
+          </div>
+          <div className={classNames(styles.priceIconGroup)}>
+            {renderPrice(price)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CafeCard;
---
+++ spire-coffee/src/component/icon/Icons.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+
+import HourglassEmptyRoundedIcon from "@mui/icons-material/HourglassEmptyRounded";
+import HourglassBottomRoundedIcon from "@mui/icons-material/HourglassBottomRounded";
+import HourglassFullRoundedIcon from "@mui/icons-material/HourglassFullRounded";
+import VolumeMuteRoundedIcon from "@mui/icons-material/VolumeMuteRounded";
+import VolumeDownRoundedIcon from "@mui/icons-material/VolumeDownRounded";
+import VolumeUpRoundedIcon from "@mui/icons-material/VolumeUpRounded";
+import AttachMoneyRoundedIcon from "@mui/icons-material/AttachMoneyRounded";
+import AccessTimeRoundedIcon from "@mui/icons-material/AccessTimeRounded";
+import LocalPhoneRoundedIcon from "@mui/icons-material/LocalPhoneRounded";
+import LanguageRoundedIcon from "@mui/icons-material/LanguageRounded";
+import LaunchRoundedIcon from "@mui/icons-material/LaunchRounded";
+
+import classNames from "classnames";
+import strings from "config/strings";
+import styles from "./Icons.module.scss";
+
+export const busyOptions = [
+  strings.list.busyness1,
+  strings.list.busyness2,
+  strings.list.busyness3,
+];
+
+export const noiseOptions = [
+  strings.list.noisiness1,
+  strings.list.noisiness2,
+  strings.list.noisiness3,
+];
+
+export function renderBusyIcon(option?: string, className?: string) {
+  if (option === busyOptions[0] || option === "LOW") {
+    return <HourglassEmptyRoundedIcon className={className} />;
+  } else if (option === busyOptions[1] || option === "MEDIUM") {
+    return <HourglassBottomRoundedIcon className={className} />;
+  } else if (option === busyOptions[2] || option === "HIGH") {
+    return <HourglassFullRoundedIcon className={className} />;
+  } else {
+    return undefined;
+  }
+}
+
+export function renderNoiseIcon(option?: string) {
+  if (option === noiseOptions[0] || option === "LOW") {
+    return (
+      <VolumeMuteRoundedIcon className={classNames(styles.noisinessIcon)} />
+    );
+  } else if (option === noiseOptions[1] || option === "MEDIUM") {
+    return (
+      <VolumeDownRoundedIcon className={classNames(styles.noisinessIcon)} />
+    );
+  } else if (option === noiseOptions[2] || option === "HIGH") {
+    return <VolumeUpRoundedIcon className={classNames(styles.noisinessIcon)} />;
+  } else {
+    return undefined;
+  }
+}
+
+export function renderPrice(option?: string) {
+  if (option === "LOW") {
+    return (
+      <div className={classNames(styles.priceIconGroup)}>
+        <AttachMoneyRoundedIcon className={classNames(styles.priceIcon)} />
+      </div>
+    );
+  } else if (option === "MEDIUM") {
+    return (
+      <div className={classNames(styles.priceIconGroup)}>
+        <AttachMoneyRoundedIcon className={classNames(styles.priceIcon)} />
+        <AttachMoneyRoundedIcon className={classNames(styles.priceIcon)} />
+      </div>
+    );
+  } else if (option === "HIGH") {
+    return (
+      <div className={classNames(styles.priceIconGroup)}>
+        <AttachMoneyRoundedIcon className={classNames(styles.priceIcon)} />
+        <AttachMoneyRoundedIcon className={classNames(styles.priceIcon)} />
+        <AttachMoneyRoundedIcon className={classNames(styles.priceIcon)} />
+      </div>
+    );
+  } else {
+    return <AttachMoneyRoundedIcon />;
+  }
+}
+
+// is there a way to make these less redundant?
+export function renderBusyText(option?: string) {
+  if (option === "LOW") {
+    return strings.list.busyness1;
+  } else if (option === "MEDIUM") {
+    return strings.list.busyness2;
+  } else if (option === "HIGH") {
+    return strings.list.busyness3;
+  } else {
+    return undefined;
+  }
+}
+
+export function renderNoiseText(option?: string) {
+  if (option === "LOW") {
+    return strings.list.noisiness1;
+  } else if (option === "MEDIUM") {
+    return strings.list.noisiness2;
+  } else if (option === "HIGH") {
+    return strings.list.noisiness3;
+  } else {
+    return undefined;
+  }
+}
+
+export function renderPriceText(option?: string) {
+  if (option === "LOW") {
+    return strings.list.priceText1;
+  } else if (option === "MEDIUM") {
+    return strings.list.priceText2;
+  } else if (option === "HIGH") {
+    return strings.list.priceText3;
+  } else {
+    return undefined;
+  }
+}
+
+export enum Icons {
+  clock = "clock",
+  phone = "phone",
+  globe = "globe",
+  redirect = "redirect",
+}
+
+export function renderCafeIcon(icon?: string) {
+  switch (icon) {
+    case Icons.clock:
+      return <AccessTimeRoundedIcon />;
+    case Icons.phone:
+      return <LocalPhoneRoundedIcon />;
+    case Icons.globe:
+      return <LanguageRoundedIcon />;
+    case Icons.redirect:
+      return <LaunchRoundedIcon />;
+    default:
+      break;
+  }
+}
