da3dd15a927b6bfd1f60e3c00766de3d1a6de291
Brandon Chan <brandonchan45@gmail.com>
2024-08-04 23:01:16 -0700

    added loading spinner

---
+++ spire-coffee/src/component/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CircularProgress, styled } from "@mui/material";
+
+const StyledCircularProgress = styled(CircularProgress)(({ theme }) => ({
+  color: theme.palette.secondary.main,
+}));
+
+const LoadingSpinner = () => {
+  return <StyledCircularProgress size={80} thickness={4.1} />;
+};
+
+export default LoadingSpinner;
