fcad0618bb1ebcec1ce93f80b3cfe1aec604ec1a
Betty Nguyen <97364242+bettyanhn@users.noreply.github.com>
2024-09-03 09:45:22 -0700

    hide footer when filter sidebar is expanded in small screen (#47)

--- spire-coffee/src/component/Body.tsx
+++ spire-coffee/src/component/Body.tsx
@@ -1,17 +1,22 @@
-import { Box } from "@mui/material";
 import Footer from "@/component/Footer";
+import { useGlobalStateManager } from "@/context/StateContext"; // Import the hook
+import { Box } from "@mui/material";
 
 type BodyPropsType = {
   children: JSX.Element | null;
 };
 
-const Body = ({ children = null }: BodyPropsType) => (
-  <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-    <Box sx={{ flex: 1 }} paddingBottom={3}>
-      {children}
+const Body = ({ children = null }: BodyPropsType) => {
+  const { showFilterSidebar, isSmallScreen } = useGlobalStateManager(); // Access the state
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <Box sx={{ flex: 1 }} paddingBottom={3}>
+        {children}
+      </Box>
+      {(!showFilterSidebar || !isSmallScreen) && <Footer />}
     </Box>
-    <Footer />
-  </Box>
-);
+  );
+};
 
 export default Body;
