202c34d842cf76ba18e5789b526c3eb72817dc59
Betty Nguyen <bettyanhn@gmail.com>
2024-09-18 21:11:04 -0700

    add available info to cafe info page

--- spire-coffee/src/pages/cafe/CafeInfo.tsx
+++ spire-coffee/src/pages/cafe/CafeInfo.tsx
@@ -5,6 +5,8 @@ import { Box, Card, CardContent, Container, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import {
+  renderBusyIcon,
+  renderNoiseIcon,
   renderPrice
 } from "../../component/icons/Icons";
 import { getCafeInfo } from "../../support/graphqlServerApi";
@@ -15,7 +17,7 @@ const CafeInfo = () => {
 
   const [cafe, setCafe] = useState<Cafe>();
 
-  const { loading, error, refetch } = useQuery(getCafeInfo, {
+  const { loading } = useQuery(getCafeInfo, {
     onError: (error) => {
       throw error;
     },
@@ -26,11 +28,6 @@ const CafeInfo = () => {
       stringId: cafeId,
     },
   });
-
-  function nameFormat(name: string | undefined) {
-    return name?.replaceAll(" ", "+").replaceAll("&", "and");
-  }
-
 
   return (
 
@@ -46,11 +43,6 @@ const CafeInfo = () => {
           ) : (
             <Box>
               <Typography variant="h1">{cafe?.name}</Typography>
-
-              {/* <div>
-                <ImageCarousel />
-              </div> */}
-
               <Card>
                 <CardContent
                   sx={{
@@ -59,79 +51,11 @@ const CafeInfo = () => {
                   }}
                 >
                   <Typography>{`${cafe?.street} ${cafe?.city}, ${cafe?.province}`}</Typography>
+                  <Typography>{renderNoiseIcon(cafe?.noisiness)}</Typography>
+                  <Typography>{renderBusyIcon(cafe?.busyness)}</Typography>
                   <Typography>{renderPrice(cafe?.price)}</Typography>
                 </CardContent>
               </Card>
-
-              <div>
-                <div>
-                  <div>
-                    {/* <Label
-                      text={`${cafe?.city}, ${cafe?.province} ${cafe?.postalCode}`}
-                    /> */}
-                    <div>
-                      {/* <Label
-                        icon={Icons.clock}
-                        text={strings.cafe.businessHours}
-                      /> */}
-                      <div>
-                        {/* {businessHours.map((hours, index) => (
-                          <div
-                            key={index}
-                            className={classNames(
-                              styles.businessHoursContainer
-                            )}
-                          >
-                            <Label text={hours.weekday} />
-                            <Label text={hours.hours} />
-                          </div>
-                        ))} */}
-                      </div>
-                    </div>
-                    {/* <Label
-                      icon={Icons.phone}
-                      text={cafe?.phoneNumber}
-                      secondaryText={strings.cafe.noPhoneNumber}
-                    /> */}
-                    {/* {renderWebsite()} */}
-                  </div>
-                  <div>
-                    {/* <Label
-                      icon={renderBusyIcon(cafe?.busyness)}
-                      text={`${strings.cafe.busynessLabel}: ${renderBusyText(
-                        cafe?.busyness
-                      )}`}
-                    /> */}
-                    {/* <Label
-                      icon={renderNoiseIcon(cafe?.noisiness)}
-                      text={`${
-                        strings.cafe.noisinessLabel
-                      }: ${renderNoiseText(cafe?.noisiness)}`}
-                    /> */}
-                    {/* <Label
-                      icon={renderPrice()}
-                      text={`${strings.cafe.priceLabel}: ${renderPriceText(
-                        cafe?.price
-                      )}`}
-                    /> */}
-                  </div>
-                </div>
-              </div>
-              <div>
-                {/* <Report cafe={cafe} /> */}
-              </div>
-              <div>
-                {
-                  <iframe
-                    width="100%"
-                    height="100%"
-                    style={{ border: 0 }}
-                    referrerPolicy="no-referrer-when-downgrade"
-                    // src={mapURL}
-                    allowFullScreen={true}
-                  />
-                }
-              </div>
             </Box>
           )}
         </React.Fragment>
