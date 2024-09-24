34194a53e78e521e5f805c98555503c40e3db266
Betty Nguyen <bnguyen47@my.bcit.ca>
2024-09-24 06:42:03 -0700

    merge conflicts

--- spire-coffee/src/pages/cafe/CafeInfo.tsx
+++ spire-coffee/src/pages/cafe/CafeInfo.tsx
@@ -1,7 +1,9 @@
-import LoadingSpinner from "@/component/LoadingSpinner";
-import strings from "@/config/strings";
+import CafeInfoCard from "@/component/cafe/CafeInfoCard";
+import ImageCarousel from "@/component/carousel/ImageCarousel";
+import { default as LoadingSpinner } from "@/component/LoadingSpinner";
+import { default as strings } from "@/config/strings";
 import { useQuery } from "@apollo/client";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCafeInfo } from "../../support/graphqlServerApi";
@@ -37,19 +39,29 @@ const CafeInfo = () => {
           ) : (
             <Box>
               <Typography variant="h1">{cafe?.name}</Typography>
-              <Card>
-                <CardContent
-                  sx={{
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                  }}
+              <Grid container spacing={2}>
+                <Grid
+                  xs={12}
+                  sm={12}
+                  md={6}
+                  lg={6}
                 >
-                  <Typography>{`${cafe?.street} ${cafe?.city}, ${cafe?.province}`}</Typography>
-                  <Typography>{renderNoiseIcon(cafe?.noisiness)}</Typography>
-                  <Typography>{renderBusyIcon(cafe?.busyness)}</Typography>
-                  <Typography>{renderPrice(cafe?.price)}</Typography>
-                </CardContent>
-              </Card>
+                  <ImageCarousel />
+                </Grid>
+                <Grid>
+                  <CafeInfoCard
+                    street={cafe?.street}
+                    city={cafe?.street}
+                    province={cafe?.province}
+                    postalCode={cafe?.postalCode}
+                    phoneNumber={cafe?.phoneNumber}
+                    website={cafe?.website}
+                    noisiness={cafe?.noisiness}
+                    busyness={cafe?.busyness}
+                    price={cafe?.price}
+                  />
+                </Grid>
+              </Grid>
             </Box>
           )}
         </React.Fragment>
